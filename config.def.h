7129c314d64cd3f300dcbb2b4b58805bdd736525
Krzysztof Maicher <krzysztof.maicher@gmail.com>
2022-05-10 07:03:32 +0200

    Change key mappings
    Use right alt instead of control key.
    Also swap h/l and j/k.

--- config.def.h
+++ config.def.h
@@ -26,11 +26,13 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	//{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	//{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
-	{ "Audacious",   NULL,       NULL,      1 << 12,       0,           -1 },
-	{ "Thunderbird", NULL,       NULL,      1 << 11,       0,           -1 }
+	/* class        instance    title         tags mask     isfloating   monitor */
+	{ "Audacious",   NULL,       NULL,        1 << 12,      0,           -1 },
+	{ "Thunderbird", NULL,       NULL,        1 << 11,      0,           -1 },
+	{ NULL,          NULL,       "notes",     1 << 9,       0,           -1 },
+	{ NULL,          NULL,       "logs",      1 << 9,       0,           -1 },
+	{ NULL,          NULL,       "devxi-1",   1,            0,           -1 },
+	{ NULL,          NULL,       "devxi-2",   1 << 1,       0,           -1 }
 };
 
 /* layout(s) */
@@ -51,9 +53,9 @@ static const Layout layouts[] = {
 #define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
+	{ MODKEY|Mod5Mask,              KEY,      toggleview,     {.ui = 1 << TAG} }, \
 	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+	{ MODKEY|Mod5Mask|ShiftMask,    KEY,      toggletag,      {.ui = 1 << TAG} },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
@@ -68,12 +70,12 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_space,  spawn,          {.v = dmenucmd }  },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_l,      focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_h,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_j,      setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_k,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,                       XK_q,      killclient,     {0} },
@@ -111,13 +113,13 @@ static const Button buttons[] = {
 	{ ClkWinTitle,          0,              Button1,        focusstack,     {.i = +1 } },
 	{ ClkWinTitle,          0,              Button2,        killclient,     {0} },
 	{ ClkWinTitle,          0,              Button3,        spawn,          {.v = dmenucmd } },
-	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
-	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
-	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+	{ ClkClientWin,         Mod5Mask,       Button1,        movemouse,      {0} },
+	{ ClkClientWin,         Mod5Mask,       Button2,        togglefloating, {0} },
+	{ ClkClientWin,         Mod5Mask,       Button3,        resizemouse,    {0} },
 	{ ClkClientWin,         0,              Button2,        killclient,     {0} },
 	{ ClkTagBar,            0,              Button1,        view,           {0} },
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
-	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
-	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+	{ ClkTagBar,            Mod5Mask,       Button1,        tag,            {0} },
+	{ ClkTagBar,            Mod5Mask,       Button3,        toggletag,      {0} },
 };
 
